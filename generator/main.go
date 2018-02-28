76a3de4f37aa7d3a56fe79187292aa9298c6d0ff
Dmitriy Matveev <dimas_os@mail.ru>
2018-02-28 17:19:46 +0200

    made gracefull shutdown. Subscription read nats, checks message, and sent it to the codeGenerator.

--- generator/main.go
+++ generator/main.go
@@ -1,30 +1,32 @@
 package main
 
 import (
-	"sync"
-	"math/rand"
-	"github.com/nats-io/go-nats-streaming"
-	"time"
 	json2 "encoding/json"
-	"strconv"
 	"fmt"
+	"github.com/nats-io/go-nats-streaming"
 	log "github.com/sirupsen/logrus"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
 	"urlcutter/generator/sub/checker"
 
+	"os"
+	"os/signal"
 )
 
 const azbuka string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const azbukaLength = len(azbuka)
-
-var codeLenght int = 4
-
-//объявляем перменную  на которую навешиваем WaitGoup, которая позволит завершить все начатые ГОрутины
+//объявляем перменную  на которую навешиваем WaitGoup, которая позволит завершить все начатые ГОрутины для создания кодов
 var wg sync.WaitGroup
+//var codeLenght int = 4
+
 //////////////////////////
 func random(min, max int) int {
 	rand.Seed(time.Now().Unix())
-	return rand.Intn(max - min) + min
+	return rand.Intn(max-min) + min
 }
+
 /////////////////////////////////////////////////////////////////
 func powerOf(length int) int {
 	if length == 0 {
@@ -47,7 +49,7 @@ func codeGenerator(codeLenght int, powerOf int, c_codes chan<- string) {
 
 	type Message struct {
 		Index string
-		Code string
+		Code  string
 	}
 
 	for i := 0; i < powerOf; i++ {
@@ -58,13 +60,13 @@ func codeGenerator(codeLenght int, powerOf int, c_codes chan<- string) {
 
 		data := &Message{
 			Index: strconv.Itoa(i),
-			Code: result}
+			Code:  result}
 
-		json, err:= json2.Marshal(data)
-		if err != nil{
+		json, err := json2.Marshal(data)
+		if err != nil {
 			fmt.Printf("Error marshal struc: &v", err)
 		}
-
+		log.Info(string(json))
 		c_codes <- string(json)
 
 		result = ""
@@ -79,16 +81,16 @@ func codeGenerator(codeLenght int, powerOf int, c_codes chan<- string) {
 	}
 	//закрываем канал что бы не расходовать ресурсы
 	close(c_codes)
-
 }
 
 ////////////////////////////////////////////////////////////////
-func connect(clientId string) stan.Conn {
-
+func connect(name string, clientId string) stan.Conn {
+	clientId += name
 	conn, err := stan.Connect("test-cluster", clientId)
 	if err != nil {
 		log.Fatalf("Cann't connect : %v, make shure NATS Streaming server is running at %s", err, clientID())
 	}
+	log.Info("Connection performed for ClientID: ", clientId)
 	return conn
 
 }
@@ -100,11 +102,8 @@ var acb = func(lguid string, err error) {
 	}
 	// Эта ф-ция будет выполнять действия последней. так как на каждое сообщение будет взят свой uuid
 	// тут мы делаем декримент от общего количества "открытых" ГОрутин
+	log.Info("Gorutine inside WG of CodeGenerator Done work")
 	wg.Done()
-	//else {
-	//	log.Printf("Received ack for msg id %s\n", lguid)
-	//}
-
 }
 
 /////////////////////////////////////////////////////////////////
@@ -122,102 +121,106 @@ func publishMsg(msg string, conn stan.Conn) {
 func clientID() string {
 	number := 1 + rand.Intn(9999-1)
 	clientId := strconv.Itoa(number)
-	log.Info("ClientID is: ", clientId)
+	//log.Info("ClientID is: ", clientId)
 	return clientId
 }
-
+/////////////////////////////////////////////////////////////////
 func main() {
 
-	c_codes := make(chan string)
 	c_startgen := make(chan int)
-	c_work := make(chan bool)
-	powerof := powerOf(codeLenght)
-//	println(powerof)
-	c_work <- true
+	c_shutdown := make(chan struct{})
+	//c_codeGenIsWork := make(chan bool)
+	var wg_gors sync.WaitGroup
 
-	log.Info("Into c_work channel sened true")
+	log.Info("Main func Started")
 
-	go func(c_startgen chan int, c_work chan bool) bool {
-	conn := connect(clientID())
+	conn := connect(clientID(), "SUB")
 	defer conn.Close()
-	defer log.Info("Connection Closed")
-	log.Info("Gorutine with Sub started")
-	for {
+	defer log.Info("SUB: Connection Closed")
+	log.Info("SUB: Gorutine started")
 
-		work := <- c_work
-		if work {
-			msgConn := func(m *stan.Msg) {
-				m.Ack()
-				log.Info("Recive data from NATS: ", m)
+	msgConn := func(m *stan.Msg) {
+		m.Ack()
+		log.Info("Recive data from NATS: ", m)
 
-				mapa := checker.Unmarshal(m.Data)
-				codeint, err := checker.Checker(mapa)
-				if err != "" {
-					log.Warn(err)
-				} else {
-					log.Info("####### CODE #########", codeint)
-					c_startgen <- codeint
+		mapa := checker.Unmarshal(m.Data)
+		codeint, err := checker.Checker(mapa)
+		if err != "" {
+			log.Warn(err)
+		}
+		log.Info("####### CODELEN #########: ", codeint)
+		c_startgen <- codeint
+	}
+
+	aw, _ := time.ParseDuration("60s")
+	_, err := conn.QueueSubscribe("dbproc", "generator", msgConn, stan.DeliverAllAvailable(), stan.SetManualAckMode(), stan.AckWait(aw), stan.DurableName("dur1"))
+
+	if err != nil {
+		fmt.Printf("Error %v\n", err)
+	}
+
+	log.Info("SUB: works")
+////////////////////////////////////////////////////////////////////////////////////////
+	go func()  {
+
+		conn := connect(clientID(), "CodeGen")
+		wg_gors.Add(1)
+		defer wg_gors.Done()
+		defer conn.Close()
+		defer log.Info("GodeGen: Connection Closed")
+		log.Info("CodeGen: Gorutine with CodeGenerator started")
+		for {
+			select {
+			case startgen :=  <-c_startgen:
+				{
+					c_codes := make(chan string)
+					//c_codeGenIsWork <- true
+					//defer close(c_codeGenIsWork)
+					log.Info("Work")
+					log.Info("Codelen: ", startgen)
+					//if startgen != 0 {
+					powerof := powerOf(startgen)
+					i := 0
+					go codeGenerator(startgen, powerof, c_codes)
+
+					for msg := range c_codes {
+						i++
+						log.Info("index of gorutines: ", i)
+						// увеличиваем количество ГОрутин, для которых надо будет дожлаться закрытие
+						wg.Add(1)
+						go publishMsg(msg, conn)
+						log.Info("Message was published: ", msg)
+						time.Sleep(300 * time.Millisecond)
+					}
+					log.Info("CodeGen: done code generation")
+				}
+
+				case  <- c_shutdown:
+				{
+					log.Info("CodeGen: Recive Shutdown signal")
+					return
 				}
 			}
 
-			aw, _ := time.ParseDuration("60s")
-			_, err := conn.QueueSubscribe("dbproc", "generator", msgConn, stan.DeliverAllAvailable(), stan.SetManualAckMode(), stan.AckWait(aw), stan.DurableName("dur1"))
-			if err != nil {
-				fmt.Printf("Error %v\n", err)
-			}
-			log.Info("Sub Gorutine works")
-			time.Sleep(5 * time.Second)
-		} else {
-			log.Info("Sub Gorutine wait for CodeGenerator finished")
-			time.Sleep(5 *time.Second)
 		}
-	}
-}(c_startgen, c_work)
+	}()
+////////////////////////////////////////////////////////////////////////////////////////////
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+
+				<- signalChan
+					log.Info("\nReceived an interrupt and closing connection...\n\n")
+
+					//conn.Close()
+					close(c_shutdown)
+					wg_gors.Wait()
+					cleanupDone <- true
 
 
-go codeGenerator(codeLenght, powerof, c_codes)
 
-
-go func(c_startgen chan int, c_work chan bool) {
-	conn := connect(clientID())
-	defer conn.Close()
-	defer log.Info("Connection Closed")
-	startgen := <- c_startgen
-	log.Info("Gorutine with CodeGenerator started")
-
-	for {
-		if startgen != 0 {
-			c_work <- false
-
-			for msg := range c_codes {
-				// увеличиваем количество ГОрутин, для которых надо будет дожлаться закрытие
-				wg.Add(1)
-				go publishMsg(msg, conn)
-				time.Sleep(300 * time.Millisecond)
-			}
-			// ждем когда отработают все ГОрутины
-			wg.Wait()
-			c_work <- true
-		} else
-		{
-			time.Sleep(5 * time.Second)
-		}
-	}
-}(c_startgen, c_work)
-
-	//
-	//signalChan := make(chan os.Signal, 1)
-	//cleanupDone := make(chan bool)
-	//signal.Notify(signalChan, os.Interrupt)
-	//go func() {
-	//	for range signalChan {
-	//		log.Info("\nReceived an interrupt and closing connection...\n\n")
-	//
-	//		//conn.Close()
-	//		cleanupDone <- true
-	//	}
-	//}()
-	//<-cleanupDone
-
+	}()
+	<- cleanupDone
 
 }
